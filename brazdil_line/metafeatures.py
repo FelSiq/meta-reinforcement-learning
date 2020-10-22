a1fc78bf2c4ae7e741bb2c776c0709f4b502fbbf
felsiq <felipe.siqueira@usp.br>
2020-10-22 17:47:10 -0300

    Correct few glitches in metafeatures module

--- brazdil_line/metafeatures.py
+++ brazdil_line/metafeatures.py
@@ -1,42 +1,43 @@
 """Metafeatures build for the Gridworld environment."""
 import typing as t
+import warnings
 
 import numpy as np
 import scipy.stats
 
 
 def ft_goal_dist_euclid(env):
-    start = env.start
-    goals = env.goals
+    start = np.asarray(list(env.start), dtype=float)
+    goals = np.asarray(list(env.goals), dtype=float)
 
-    dists = np.linalg.norm(np.array(goals, dtype=float) - start, ord=2, axis=0)
+    dists = np.linalg.norm(goals - start, ord=2, axis=0)
 
     return dists
 
 
 def ft_goal_dist_manh(env):
-    start = env.start
-    goals = env.goals
+    start = np.asarray(list(env.start), dtype=float)
+    goals = np.asarray(list(env.goals), dtype=float)
 
-    dists = np.linalg.norm(np.array(goals, dtype=float) - start, ord=1, axis=0)
+    dists = np.linalg.norm(goals - start, ord=1, axis=0)
 
     return dists
 
 
 def ft_trap_dist_euclid(env):
-    start = env.start
-    traps = env.traps
+    start = np.asarray(list(env.start), dtype=float)
+    traps = np.asarray(list(env.traps), dtype=float)
 
-    dists = np.linalg.norm(np.array(traps, dtype=float) - start, ord=2, axis=0)
+    dists = np.linalg.norm(traps - start, ord=2, axis=0)
 
     return dists
 
 
 def ft_trap_dist_manh(env):
-    start = env.start
-    traps = env.traps
+    start = np.asarray(list(env.start), dtype=float)
+    traps = np.asarray(list(env.traps), dtype=float)
 
-    dists = np.linalg.norm(np.array(traps, dtype=float) - start, ord=1, axis=0)
+    dists = np.linalg.norm(traps - start, ord=1, axis=0)
 
     return dists
 
@@ -46,20 +47,18 @@ def _in_radius_vals(env, values: np.ndarray, radius_prop: float):
 
     width = env.width
     height = env.width
-    start = env.start
-    traps = env.traps
 
     radius = radius_prop * min(width, height)
 
     return values[values <= radius]
 
 
-def ft_goal_radial_dist(env, radius_prop: float):
+def ft_goal_radial_dist(env, radius_prop: float = 0.2):
     euclid_dists = ft_goal_dist_euclid(env)
     return _in_radius_vals(env, values=euclid_dists, radius_prop=radius_prop)
 
 
-def ft_trap_radial_dist(env, radius_prop: float):
+def ft_trap_radial_dist(env, radius_prop: float = 0.2):
     euclid_dists = ft_trap_dist_euclid(env)
     return _in_radius_vals(env, values=euclid_dists, radius_prop=radius_prop)
 
@@ -80,6 +79,9 @@ summary_functions = {
 def summarize(
     feature: str, values: t.Union[float, np.ndarray]
 ) -> t.Tuple[t.List[str], t.List[float]]:
+    if feature.startswith("ft_"):
+        feature = feature[3:]
+
     if np.isscalar(values):
         return [feature], [values]
 
@@ -87,14 +89,21 @@ def summarize(
     mtf_vals = []  # type: t.List[float]
 
     for summ_name, summ_func in summary_functions.items():
-        mtf_vals.append(summ_func(values))
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                mtf_vals.append(summ_func(values))
+
+        except ValueError:
+            mtf_vals.append(np.nan)
+
         mtf_names.append(f"{feature}.{summ_name}")
 
     return mtf_names, mtf_vals
 
 
 def _test():
-    ft = ["ab", "cd", "ef"]
+    ft = ["ab", "ft_cd", "ft_ef"]
     vals = [[1.0, 2.0], [-7, 7], 0]
 
     for ft_name, ft_vals in zip(ft, vals):
