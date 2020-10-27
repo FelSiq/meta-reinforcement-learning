ed8aa6290f04eb19b7ae8797b3f92bf5ef0d5cd4
felsiq <felipe.siqueira@usp.br>
2020-10-27 16:52:06 -0300

    Add function to combine performance and stds

--- brazdil_line/build_metamodel.py
+++ brazdil_line/build_metamodel.py
@@ -1,3 +1,4 @@
+import typing as t
 import argparse
 
 import matplotlib.pyplot as plt
@@ -56,6 +57,7 @@ np.random.seed(16)
 random_seeds = np.random.randint(1000, size=y.shape[1])
 KFold = sklearn.model_selection.KFold(n_splits=10)
 
+
 for i in np.arange(num_base_models):
     print(f"Fitting {i + 1}th model...", end=" ")
 
@@ -85,7 +87,71 @@ for i in np.arange(num_base_models):
 
     print("Done.")
 
-print("Labels standard deviation:", y.std(axis=0))
-print("Labels converged class distribution:", (y > 0.0).mean(axis=0))
-print(res_reg)
-print(res_cls)
+
+def combine_stats(
+    means: np.ndarray, stds: np.ndarray, sample_size_per_group: int
+) -> t.Tuple[float, float]:
+    num_groups = means.size
+
+    combined_mean = np.mean(means)
+
+    total_inst = num_groups * sample_size_per_group
+
+    total_var = (
+        sample_size_per_group * np.sum(np.square(means - combined_mean))
+        + (sample_size_per_group - 1) * np.sum(np.square(stds))
+    ) / (total_inst - 1)
+
+    return combined_mean, np.sqrt(total_var)
+
+
+for i, alg in enumerate(y.columns):
+    print("Algorithm:", alg)
+
+    train_mean_cls, train_std_cls, test_mean_cls, test_std_cls = res_cls[i].values.T
+
+    combined_train_mean_cls, combined_train_std_cls = combine_stats(
+        means=train_mean_cls, stds=train_std_cls, sample_size_per_group=y.shape[0]
+    )
+    combined_test_mean_cls, combined_test_std_cls = combine_stats(
+        means=test_mean_cls, stds=test_std_cls, sample_size_per_group=y.shape[0]
+    )
+
+    print(
+        "  classification - Accuracy by random chance :",
+        np.mean(y.iloc[:, i].values > 0.0),
+    )
+    print(
+        "  classification - Train accuracy / std      :",
+        1.0 - combined_train_mean_cls,
+        combined_train_std_cls,
+    )
+    print(
+        "  classification - Test accuracy / std       :",
+        1.0 - combined_test_mean_cls,
+        combined_test_std_cls,
+    )
+
+    train_mean_reg, train_std_reg, test_mean_reg, test_std_reg = res_reg[i].values.T
+
+    combined_train_mean_reg, combined_train_std_reg = combine_stats(
+        means=train_mean_reg, stds=train_std_reg, sample_size_per_group=y.shape[0]
+    )
+    combined_test_mean_reg, combined_test_std_reg = combine_stats(
+        means=test_mean_reg, stds=test_std_reg, sample_size_per_group=y.shape[0]
+    )
+
+    print(
+        "  regression     - target standard dev       :",
+        np.std(y.iloc[:, i].values, ddof=1),
+    )
+    print(
+        "  regression     - Train rmse / std          :",
+        combined_train_mean_reg,
+        combined_train_std_reg,
+    )
+    print(
+        "  regression     - Test rmse / std           :",
+        combined_test_mean_reg,
+        combined_test_std_reg,
+    )
